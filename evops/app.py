5b3f58440cb57e457e115dc26166b095000a5862
hng12 <58843937+hng12@users.noreply.github.com>
2023-02-09 16:51:13 +0000

    adfasdf

--- evops/app.py
+++ evops/app.py
@@ -2,18 +2,24 @@ from flask import Flask, render_template
 from main import get_csv
 import pandas as pd
 import numpy as np
+from liveserver import LiveServer
 
 #get_csv("Hampshire")
 # add dials for changing parameters
 # matplotlib graphs per generation
 df = pd.read_csv('optimal.csv')
-
+lats = df.iloc[:,0]
+lons = df.iloc[:,1]
+lat = np.median(lats)
+lon = np.median(lons)
 app = Flask(__name__)
 markers = df.to_numpy()
+ls = LiveServer(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html', markers=markers)
+    return render_template('index.html', markers=markers, lat=lat, lon=lon)
 
-if __name__ == '__main__':
-    app.run()
+#if __name__ == '__main__':
+# ls.run("0.0.0.0", 8080)
+app.run()
--- evops/static/js/script.js
+++ evops/static/js/script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').fitWorld();
+var map = L.map('map')
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
