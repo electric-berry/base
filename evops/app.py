b98d31ebb86d932eebc60681f645859556b6bedc
hng12 <58843937+hng12@users.noreply.github.com>
2023-02-09 11:38:50 +0000

    map now displays markers

--- evops/app.py
+++ evops/app.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template
 from main import get_csv
 import pandas as pd
+import numpy as np
 
 #get_csv("Hampshire")
+# add dials for changing parameters
+# matplotlib graphs per generation
 df = pd.read_csv('optimal.csv')
 
-
 app = Flask(__name__)
-markers = df.to_dict()
-print(markers)
+markers = df.to_numpy()
 
 @app.route('/')
 def index():
