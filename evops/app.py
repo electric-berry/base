f73f4ffa7ec7c157c7eab06af10152545d8547bf
hng12 <henrywangngai@hotmail.com>
2023-02-09 09:17:35 +0000

    Change to website with Flask

--- evops/app.py
+++ evops/app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from main import get_csv
+import pandas as pd
+
+get_csv("Hampshire")
+df = pd.read_csv('optimal.csv')
+
 app = Flask(__name__)
 
-
-@app.route('/test')
-def hello():
-    return "Hello World!"
+@app.route('/')
+def index():
+    return(df.to_string())
 
 if __name__ == '__main__':
     app.run()
